8d1a9118bf7cc85a31b6d4628c36e2bb0f2f1b8a
Andy Brown <6717533+andycymru@users.noreply.github.com>
2019-01-09 12:34:14 +0000

    the files

---
+++ wp-plugin-name.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+* Plugin Name: Starkers Plugin Template
+* Plugin URI: http://onlinewebtutor.com
+* Description: This is the starkers of creating a plugin
+* Author: Andy Brown
+* Author URI: http://andy.cymru
+* Version: 1.0
+*/
+
+?>
